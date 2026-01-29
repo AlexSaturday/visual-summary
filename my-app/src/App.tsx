04720fab4b587b6906c76ac3b2bcf8d41846df30
AlexSaturday <xsuturinx@yandex.ru>
2026-01-29 12:33:41 +0300

    first deploy ver

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
 import './App.css';
 import About from './components/About/About';
 import Header from './components/Header/Header';
-import Links from './components/Links/Links';
 import Projects from './components/Projects/Projects';
 import Footer from './components/Footer/Footer';
 
